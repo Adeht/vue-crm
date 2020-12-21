7a211def8487a101252417e7bca86e72d6f8299a
ADehtHome <ad@edeg.xyz>
2020-12-21 22:07:33 +0200

    урок 16

--- src/store/category.js
+++ src/store/category.js
@@ -28,6 +28,17 @@ export default {
                 throw e
             }
         },
+        async fetchCategoryByID({commit, dispatch}, id) {
+            try {
+                const uid = await dispatch('getUid')
+                const category = (await firebase.database().ref(`users/${uid}/categories`).child(id).once('value')).val() || {}
+
+                return {...category, id}
+            } catch (e) {
+                commit('setError', e)
+                throw e
+            }
+        },
         async updateCategory({commit, dispatch}, {title, limit, id}) {
             try {
                 const uid = await dispatch('getUid')
